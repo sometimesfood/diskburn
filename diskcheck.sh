73e042be84559a988ae6e9e7016a38db371fb6b7
Jörg Herzinger <joerg.herzinger@oiml.at>
2013-12-30 09:08:57 +0000

    Major improvements to diskcheck.sh
    - Add more read and write checks
    - Add zcav plot
    - Add logging

--- diskcheck.sh
+++ diskcheck.sh
@@ -1,33 +1,93 @@
 #!/bin/bash
 
-set -e
-
 if [ $# -eq 0 ]
 then
     echo "Usage: diskcheck.sh DEVICE..."
     exit 1
 fi
 
-function smartcheck {
-    local disk=$1
-    local check_no=$2
-    local basename=$(basename $disk)
-    smartctl -d sat --all $disk > ${basename}.smart.${check_no}
-}
+DATE=$(date +%FT%R)
+mkdir $DATE; cd $DATE
 
-function checkdisk {
-    local disk=$1
-    local basename=$(basename ${disk})
-    smartcheck $disk 1
-    badblocks -sv -o ${basename}.bb.ro ${disk}
-    smartcheck $disk 2
-    badblocks -svw -o ${basename}.bb.rw ${disk}
-    smartcheck $disk 3
-    zcav $disk > ${basename}.zcav
-    smartcheck $disk 4
-}
-
-for disk in "$@"
-do
-    checkdisk $disk
+for prog in badblocks smartctl zcav pgrep gnuplot
+do 
+    which $prog > /dev/null || echo "${prog} is not installed\!"
 done
+function thetime {
+    date +%c
+}
+
+function smartcheck {
+    local check_no=$1
+    shift
+    for disk in $@
+    do
+	local basename=$(basename $disk)
+	echo "Running SMART #${check_no} of ${basename} on $(thetime)"
+	smartctl -d sat --all $disk > ${basename}.smart.${check_no}
+    done
+}
+
+function bbcheck {
+    local mode=$1
+    shift
+    test $mode = "rw" && local opt="-w" || local opt=""
+    echo "Running badblocks with mode ${mode} for all disks on $(thetime)"
+    for disk in $@
+    do
+	local basename=$(basename $disk)
+	badblocks ${opt} -o ${basename}.bb.${mode} ${disk}&
+    done
+    while pgrep 'badblocks' > /dev/null;do sleep 60;done
+}
+
+function zcavcheck {
+    local mode=$1
+    shift
+    test $mode = "rw" && local opt="-w" || local opt=""
+    for disk in $@
+    do
+	local basename=$(basename $disk)
+	echo "Running zcav with mode ${mode} of disk ${basename} on $(thetime)"
+	zcav ${opt} -l ${basename}.zcav.${mode} ${disk}
+    done
+}
+
+function draw_zcav {
+    for mode in ro rw;
+    do
+	for disk in $@;
+	do
+	    local basename=$(basename $disk)
+            echo "unset autoscale x
+set autoscale xmax
+set autoscale xmin
+unset autoscale y
+set autoscale ymax
+set autoscale ymin
+set xlabel \"Position MB\"
+set ylabel \"MB/s\"
+set terminal png
+set output \"${basename}.zcav.${mode}.png\"
+plot \"${basename}.zcav.${mode}\" with dots" | gnuplot
+	done
+    done
+}
+
+{
+echo "Starting diskceck on $(thetime)"
+echo "Disks checked are $@"
+smartcheck 1 "$@"
+bbcheck ro "$@"
+smartcheck 2 "$@"
+bbcheck rw "$@"
+smartcheck 3 "$@"
+zcavcheck ro "$@"
+smartcheck 4 "$@"
+zcavcheck rw "$@"
+smartcheck 5 "$@"
+draw_zcav "$@"
+echo "Finished diskceck on $(thetime)"
+} &> diskcheck.log
+
+cd ..
