b598088ef57cd6da91820982e138b04214729e0e
Sebastian Boehm <sebastian@sometimesfood.org>
2014-01-08 12:49:22 +0100

    Use spaces instead of tabs

--- diskcheck.sh
+++ diskcheck.sh
@@ -29,9 +29,9 @@ function smartcheck {
     local check_no=$1
     shift
     for disk in $@; do
-	local basename=$(basename $disk)
-	log "Running SMART check #${check_no} on ${basename}"
-	smartctl -d sat --all $disk > ${basename}.smart.${check_no}
+        local basename=$(basename $disk)
+        log "Running SMART check #${check_no} on ${basename}"
+        smartctl -d sat --all $disk > ${basename}.smart.${check_no}
     done
 }
 
@@ -40,25 +40,25 @@ function bbcheck {
     shift
     test $mode = "rw" && local opt="-w" || local opt=""
     for disk in $@; do
-	local basename=$(basename $disk)
+        local basename=$(basename $disk)
         log "Checking $basename for bad blocks ($mode mode)"
-	badblocks ${opt} -o ${basename}.bb.${mode} ${disk}&
+        badblocks ${opt} -o ${basename}.bb.${mode} ${disk}&
     done
     wait
 }
 
 function zcavcheck {
     for disk in $@; do
-	local basename=$(basename $disk)
-	log "Running zcav on ${basename}"
-	zcav -l ${basename}.zcav ${disk}
+        local basename=$(basename $disk)
+        log "Running zcav on ${basename}"
+        zcav -l ${basename}.zcav ${disk}
     done
 }
 
 function draw_zcav {
     for disk in $@; do
-	local basename=$(basename $disk)
-	echo "unset autoscale x
+        local basename=$(basename $disk)
+        echo "unset autoscale x
 set autoscale xmax
 set autoscale xmin
 unset autoscale y
