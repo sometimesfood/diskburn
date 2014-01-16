0a8a7eb2107a8fafaa2142e16eec5718207cd3f9
Sebastian Boehm <sebastian@sometimesfood.org>
2014-01-16 11:27:51 +0100

    Refactor main method

--- diskcheck.sh
+++ diskcheck.sh
@@ -34,36 +34,32 @@ err_exit() {
 }
 
 smartcheck() {
-    local check_no=$1
-    shift
-    for disk in $@; do
-        local basename=$(basename ${disk})
-        log "Running SMART check #${check_no} on ${disk}"
-        smartctl -d sat --all ${disk} > ${basename}.smart.${check_no}
-    done
+    local check_no="$1"
+    local disk="$2"
+    local basename="$(basename ${disk})"
+    log "Running SMART check #${check_no} on ${disk}"
+    smartctl -d sat --all ${disk} > ${basename}.smart.${check_no}
 }
 
 bbcheck() {
-    for disk in $@; do
-        local basename=$(basename ${disk})
-        log "Checking ${disk} for bad blocks"
-        badblocks -w -o ${basename}.bb ${disk} &
-    done
-    wait
+    local disk="$1"
+    local basename="$(basename ${disk})"
+    log "Checking ${disk} for bad blocks..."
+    badblocks -w -o ${basename}.bb ${disk} &
 }
 
 zcavcheck() {
-    local mode=$1
-    shift
-    [[ ${mode} = "rw" ]] && local opt="-w" || local opt=""
-    for disk in $@; do
-        local basename=$(basename ${disk})
-        log "Running zcav on ${disk} (${mode} mode)"
-        zcav ${opt} -l ${basename}.${mode}.zcav ${disk}
-    done
+    local mode="$1"
+    local disk="$2"
+    local basename="$(basename ${disk})"
+    [[ ${mode} = "write" ]] && local opt="-w" || local opt=""
+    log "Running zcav on ${disk} (${mode} mode)"
+    zcav ${opt} -l ${basename}.${mode}.zcav ${disk}
 }
 
 draw_zcav() {
+    local disk="$1"
+    local basename="$(basename ${disk})"
     cat <<EOF > gnuplot.gp
 unset autoscale x
 set autoscale xmax
@@ -76,16 +72,13 @@ set ylabel "transfer rate (MB/s)"
 set terminal png size 2560,960
 set output disk.".zcav.png"
 set multiplot layout 1,2 title disk
-set title "ro"
-plot disk.".ro.zcav" pt 1 ps 1
-set title "rw"
-plot disk.".rw.zcav" pt 1 ps 1
+set title "reads"
+plot disk.".read.zcav" pt 1 ps 1
+set title "writes"
+plot disk.".write.zcav" pt 1 ps 1
 unset multiplot
 EOF
-    for disk in $@; do
-        local basename=$(basename ${disk})
-        which gnuplot &> /dev/null && gnuplot -e "disk='${basename}'" gnuplot.gp
-    done
+    which gnuplot &> /dev/null && gnuplot -e "disk='${basename}'" gnuplot.gp
 }
 
 main() {
@@ -96,17 +89,24 @@ main() {
     local basedir="diskcheck-$(date +%FT%T)"
     mkdir ${basedir}
     pushd ${basedir}
-
     log "Starting diskcheck on $@"
-    smartcheck 1 "$@"
-    bbcheck "$@"
-    smartcheck 2 "$@"
-    zcavcheck ro "$@"
-    smartcheck 3 "$@"
-    zcavcheck rw "$@"
-    smartcheck 4 "$@"
-    draw_zcav "$@"
-    log "Finished diskcheck"
+
+    for disk in $@; do
+        smartcheck 1 "${disk}"
+        bbcheck "${disk}"
+    done
+    wait
+
+    for disk in $@; do
+        smartcheck 2 "${disk}"
+        zcavcheck read "${disk}"
+        smartcheck 3 "${disk}"
+        zcavcheck write "${disk}"
+        smartcheck 4 "${disk}"
+        draw_zcav "${disk}"
+    done
+
+    log "Finished diskcheck on $@"
     popd
 }
 
